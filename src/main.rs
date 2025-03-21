8299432212faeec4b255081e2ded0023201cba2a
onelrian <onelsob57@gmail.com>
2025-03-21 09:24:09 +0100

    Init: Integrated modular alarm system in main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,9 @@ mod sounds;
 mod repeat;
 mod utils;
 
-use alarm::set_alarm;
-use sounds::choose_sound;
-use repeat::choose_repeat_mode;
-use utils::prompt_user;
+use alarm::Alarm;
 
 fn main() {
-    // Get alarm time from user
-    let alarm_time = prompt_user("Enter alarm time (HH:MM):");
-
-    // Select alarm sound
-    let sound = choose_sound();
-
-    // Choose repeat mode
-    let repeat_mode = choose_repeat_mode();
-
-    // Set and run alarm
-    set_alarm(&alarm_time, &sound, repeat_mode);
+    let alarm = Alarm::set_alarm();
+    alarm.wait_until_alarm();
 }
