85c7c83620fbc1b5e65006913b38af7e19dda293
onelrian <onelsob57@gmail.com>
2025-03-21 09:47:41 +0100

    Refactor: Updated main process to properly invoke background alarm process

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,15 @@ mod repeat;
 mod utils;
 
 use alarm::Alarm;
+use sounds::choose_sound;
+use repeat::choose_repeat_mode;
+use utils::prompt_user;
 
 fn main() {
-    let alarm = Alarm::set_alarm();
-    alarm.wait_until_alarm();
+    let alarm_time = prompt_user("Enter alarm time (HH:MM):");
+    let sound = choose_sound();
+    let repeat_mode = choose_repeat_mode();
+    
+    let alarm = Alarm::new(&alarm_time, &sound, repeat_mode);
+    alarm.start_in_background();
 }
