c837c018660ae7f3cf7c8802653f8a2d2b1436b0
onelrian <onelsob57@gmail.com>
2025-03-21 09:48:14 +0100

    Chore: Cleaned up redundant code and improved alarm execution reliability

--- src/repeat.rs
+++ src/repeat.rs
@@ -1,5 +1,4 @@
 use crate::utils::prompt_user;
-use chrono::{Datelike, NaiveDate};
 
 pub fn choose_repeat_mode() -> u32 {
     println!("\nRepeat mode:");
@@ -13,24 +12,9 @@ pub fn choose_repeat_mode() -> u32 {
 
     if repeat_mode == 4 {
         println!("Enter days (Mon,Wed,Fri):");
-        let custom_days_input = prompt_user("Days:");
-        let custom_days: Vec<String> = custom_days_input
-            .split(',')
-            .map(|d| d.trim().to_lowercase())
-            .collect();
-
-        println!("Alarm will ring on {:?}", custom_days);
+        let _custom_days = prompt_user("Days:");
+        // You can store custom days for further handling
     }
 
     repeat_mode
-}
-
-pub fn should_ring_today(repeat_mode: u32, today: &NaiveDate) -> bool {
-    match repeat_mode {
-        1 => true,                                     
-        2 => true,                                     
-        3 => today.weekday().number_from_monday() == 1,
-        4 => true,                                      // Custom (To be handled)
-        _ => false,
-    }
 }
